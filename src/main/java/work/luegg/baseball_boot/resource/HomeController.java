1ff7944986e8cbd825516534587988d0d3310508
庫曼 <@192.168.68.112>
2026-07-07 23:15:36 +0800

    增加根目錄的controller

---
+++ src/main/java/work/luegg/baseball_boot/resource/HomeController.java
@@ -0,0 +1,13 @@
+package work.luegg.baseball_boot.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/html/login.html";
+    }
+}
